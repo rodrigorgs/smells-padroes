4a2d7efc62e0ebba865c9269d4eacced52a982dc
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-01-05 23:04:50 -0300

    Combina diversos projetos na análise.

--- analisa-delta.R
+++ analisa-delta.R
@@ -4,16 +4,78 @@ library(dplyr)
 
 # project <- "displaytag"
 # project <- "commons-io"
-project <- "fastjson"
+projects <- c("fastjson", "displaytag", "commons-io", "commons-collections")
 
 # Carrega dados
-BASE_PATH <- "~/Dropbox/ARTIGOS/arquivos_2020/arquivos_para_analise/"
-padroes_orig <- read.csv(path.expand(paste0(BASE_PATH, project, "/", project, "_padrao.txt")), sep = "\t", header = F, stringsAsFactors = F,
-  col.names = c("commit", "parent", "ancestor",  "classe", "metodo", "padrao", "operacao"))
-smells_orig <- read.csv(path.expand(paste0(BASE_PATH, project, "/", project, "_smells.txt")), sep = "\t", header = F, stringsAsFactors = F,
-  col.names = c("commit", "parent", "ancestor", "classe", "metodo", "smell", "operacao"))
+SAVE_FILE = "padroes_smells.RData"
+if (file.exists(SAVE_FILE)) {
+  load(SAVE_FILE)
+} else {
+  BASE_PATH <- "~/Dropbox/ARTIGOS/arquivos_2020/arquivos_para_analise/"
+  
+  padroes_orig <- NULL
+  smells_orig <- NULL
+  for (project in projects) {
+    print(project)
+    
+    x <- read.csv(path.expand(paste0(BASE_PATH, project, "/", project, "_padrao.txt")), sep = "\t", header = F, stringsAsFactors = F,
+             col.names = c("commit", "parent", "ancestor",  "classe", "metodo", "padrao", "operacao")) %>% mutate(project = project)
+    padroes_orig <- padroes_orig %>% bind_rows(x)
+    
+    x <- read.csv(path.expand(paste0(BASE_PATH, project, "/", project, "_smells.txt")), sep = "\t", header = F, stringsAsFactors = F,
+                  col.names = c("commit", "parent", "ancestor", "classe", "metodo", "smell", "operacao")) %>% mutate(project = project)
+    smells_orig <- smells_orig %>% bind_rows(x)
+  }
+  
+  save(padroes_orig, smells_orig, file = SAVE_FILE)
+}
 
-# Remove registros sem commit anterior
+##########
+
+#' # Estatísticas
+
+#' Número de commits analisados
+
+n_distinct(padroes_orig$commit)
+n_distinct(smells_orig$commit)
+
+#' Número de classes analisadas
+# TODO: são somente as classes que tiveram algum smell ou algum padrão em algum momento?
+
+n_distinct(padroes_orig$classe)
+n_distinct(smells_orig$classe)
+
+padroes_orig %>% group_by(project) %>% summarise(n_distinct(classe))
+padroes_orig %>% group_by(project, classe) %>% summarise(n())
+
+n_distinct(padroes_orig$classe)
+n_distinct(smells_orig$classe)
+
+#' Ocorrências de adição de padrão/smell
+
+sum(padroes_orig$operacao == "Adicionado")
+sum(smells_orig$operacao == "Adicionado")
+
+#' Ocorrências de adição de padrão/smell por projeto
+
+padroes_orig %>% 
+  filter(operacao == "Adicionado") %>%
+  group_by(project) %>%
+  tally() %>%
+  arrange(desc(n))
+
+smells_orig %>% 
+  filter(operacao == "Adicionado") %>%
+  group_by(project) %>%
+  tally() %>%
+  arrange(desc(n))
+
+
+##########
+
+# Remove registros sem commit anterior e considera somente alterações
+# (padrão/smell adicionado/removido)
+
 padroes <- padroes_orig %>%
   filter(ancestor != 'null' & parent != 'null') %>%
   filter(parent == ancestor) %>%
@@ -29,20 +91,19 @@ smells <- smells_orig %>%
 
 ##########
 
-x <- padroes %>% filter(operacao != 'Passado')
 #' Quantas adições/remoções de padrões?
 padroes %>% filter(operacao != 'Passado') %>% tally()
 
 #' Quantas adições/remoções de smells?
 smells %>% filter(operacao != 'Passado') %>% tally()
 
-
 ##########
 
 data <- padroes %>%
   rename(operacao_padrao = operacao) %>%
   filter(operacao_padrao == 'Adicionado') %>%
-  left_join(smells)
+  left_join(smells) %>%
+  rename(operacao_smell = operacao)
 
 xtabs(~ padrao + smell, data=data, exclude=NULL, na.action=na.pass)
 
@@ -52,10 +113,10 @@ xtabs(~ padrao + smell, data=data, exclude=NULL, na.action=na.pass)
 
 data2 <- data %>%
   group_by(commit, parent, classe, metodo, padrao) %>%
-  summarise(added_smell = any(operacao == 'Adicionado') %>% coalesce(FALSE),
-            removed_smell = any(operacao == 'Removido') %>% coalesce(FALSE))
+  summarise(added_smell = any(operacao_smell == 'Adicionado') %>% coalesce(FALSE),
+            removed_smell = any(operacao_smell == 'Removido') %>% coalesce(FALSE))
 
-#' Ao adicionar um determinado padrão P, quantos % das vezes estamos adicionando algum smell (no mesmo commit)?
+#' # Ao adicionar um determinado padrão P, quantos % das vezes estamos adicionando algum smell (no mesmo commit)?
 t <- xtabs(~ padrao + added_smell, data=data2, exclude=NULL, na.action=na.pass)
 t
 mosaicplot(t, shade=T)
@@ -65,3 +126,19 @@ chisq.test(t)
 # t
 # mosaicplot(t, shade=T)
 # chisq.test(t)
+
+##############
+
+#' # Quais são os smells mais frequentes nos padrões State e Template Method?
+
+data3 <- data %>%
+  filter(padrao %in% c("State", "Template Method")) %>%
+  filter(operacao_padrao == "Adicionado") %>%
+  filter(is.na(operacao_smell) | operacao_smell != "Removido")
+
+t <- xtabs(~ padrao + smell, data = data3, exclude=NULL, na.action=na.pass)
+t
+mosaicplot(t, shade=T)
+chisq.test(t)
+
+data3 %>% filter(operacao_smell == "Adicionado")
